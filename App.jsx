91d42897e3cd8b7080fa1ea8806b5e662004ffda
Shaili11 <shailisaini08@gmail.com>
2024-11-19 11:39:38 +0530

    My first project

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Weather from './components/W'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Weather/>
+      
+    </div>
+  )
+}
+
+export default App
+
